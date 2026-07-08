b43f10aca59d2ae10f33552818378a78a04a4838
gencege8 <gencege8@gmail.com>
2026-07-08 14:42:44 +0300

    Change in bookcontroller api endpoint

--- src/main/java/com/ege/bookstore/controller/BookController.java
+++ src/main/java/com/ege/bookstore/controller/BookController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/books")
+@RequestMapping("/api/book1")
 public class BookController {
 
     private final BookService service;     // controller injects SERVICE,
