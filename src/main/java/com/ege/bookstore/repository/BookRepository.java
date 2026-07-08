d73358b2946ad353f5af2895fc0d751e5da36022
gencege8 <gencege8@gmail.com>
2026-07-08 14:20:54 +0300

    Removed unused imports and fix mapping inconsistancy

--- src/main/java/com/ege/bookstore/repository/BookRepository.java
+++ src/main/java/com/ege/bookstore/repository/BookRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     List<Book> findByAuthor(String author);
-    List<Book> findByPriceLessThan(double price);
+    
     Optional<Book> findByTitle(String title);
 
 }
